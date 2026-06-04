8272e78c3865a2e830c85ce0d08ef9429ff6d501
gafs-codex <abdulmuizgafarcodes@gmail>
2026-06-04 18:39:11 -0400

    re-examining the docker page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './Components/Navbar/Navbar';
-import Landing_Page from './Components/Landing_Page/Landing_Page';
-import Sign_Up from './Components/Sign_Up/Sign_Up';
-import Login from './Components/Login/Login';
-import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
-import Notification from './Components/Notification/Notification'; // Standardized to 'Components'
-import ProfileCard from './Components/ProfileCard/ProfileCard';
-import ReportsLayout from './Components/ReportsLayout/ReportsLayout';
+import Navbar from './components/Navbar/Navbar';
+import Landing_Page from './components/Landing_Page/Landing_Page';
+import Sign_Up from './components/Sign_Up/Sign_Up';
+import Login from './components/Login/Login';
+import InstantConsultation from './components/InstantConsultationBooking/InstantConsultation';
+import Notification from './components/Notification/Notification'; // Standardized to 'Components'
+import ProfileCard from './components/ProfileCard/ProfileCard';
+import ReportsLayout from './components/ReportsLayout/ReportsLayout';
 
 function App() {
   return (
